cf6eed222ec2b59c8ce91c4e547177719d2286ea
johan-l-r <188136149+johan-l-r@users.noreply.github.com>
2025-05-15 08:51:09 -0500

    feat: add normalization of player movement
    - added normalization for diagnoal movement

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include "SDL3/SDL.h"
+#include <cmath>
+#include <iostream>
 
 void checkSubsystemStatus(SDL_InitFlags subsystem) {
   // check if there where any errors when starting a subsystem 
@@ -27,10 +29,13 @@ int main() {
   unsigned const int PLAYER_WIDTH = 50;
   unsigned const int PLAYER_HEIGHT = 50;
 
+	int playerXDirection = 0;
+	int playerYDirection = 0;
+
   float playerXPos = (WINDOW_WIDTH - PLAYER_WIDTH) / 2; 
   float playerYPos = (WINDOW_HEIGHT - PLAYER_HEIGHT) / 2; 
 
-  float playerSpeed = 0.2;
+  float playerSpeed = 0.05;
 
   SDL_Window *window = NULL; 
   SDL_Renderer *renderer = NULL; 
@@ -62,10 +67,28 @@ int main() {
       }
     }
 
-    if (keys[SDL_SCANCODE_W]) player.y -= playerSpeed;
-    if (keys[SDL_SCANCODE_S]) player.y += playerSpeed;
-    if (keys[SDL_SCANCODE_A]) player.x -= playerSpeed;
-    if (keys[SDL_SCANCODE_D]) player.x += playerSpeed;
+    if (keys[SDL_SCANCODE_W]) playerYDirection = -1;
+    if (keys[SDL_SCANCODE_S]) playerYDirection = 1;
+    if (keys[SDL_SCANCODE_A]) playerXDirection = -1;
+    if (keys[SDL_SCANCODE_D]) playerXDirection = 1;
+
+    // vector normalization
+		float magnitud = sqrt(pow(playerXDirection, 2) + pow(playerYDirection, 2));
+
+    if(magnitud != 0) {
+      float normalizedX = playerXDirection / magnitud;
+      float normalizedY = playerYDirection / magnitud;
+
+      playerXPos += normalizedX * playerSpeed;
+      playerYPos += normalizedY * playerSpeed;
+    }
+
+    player.x = playerXPos;
+    player.y = playerYPos;
+
+    // reset direction when no keys are pressed 
+    playerXDirection = 0;
+    playerYDirection = 0;
 
     // set background color
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
